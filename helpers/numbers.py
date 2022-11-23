55224ef6e2701854834f7807a538fd9a3c524a1d
JLeitzinger <elaboratejohnny@gmail.com>
2022-11-23 13:01:34 -0500

    added prime check

--- helpers/numbers.py
+++ helpers/numbers.py
@@ -1,4 +1,4 @@
-
+from math import sqrt
 arr = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 73, 79, 83, 89, 97, 101]
 
 
@@ -12,7 +12,32 @@ def diffArray(arr):
 
     return diffArray
 
-def numToLetters(arr):
+def numToLetters(number):
 
-    return [chr(ord('@')+x) for x in arr]
+    return chr(ord('@')+number)
 
+def checkPrime(number):
+    
+    primeFlag = 0
+
+    if(number > 1):
+        for i in range(2, int(sqrt(number)) + 1):
+            if (number % i == 0):
+                primeFlag = 1
+                break
+        if (primeFlag == 0):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def checkArray(arr, check):
+    if (check=='letters'):
+        return [numToLetters(x) for x in arr]
+    elif (check=='prime'):
+        return [checkPrime(x) for x in arr]
+    elif (check=='difference'):
+        return diffArray(arr)
+    else:
+        print('No Valid Selection')
